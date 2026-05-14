ef997d11f4c7cf82276b9b04537c3bf8019db558
Barsbek Azamatov <bars.moderside@gmail.com>
2026-05-14 14:32:23 -0700

    add sum_ascii

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
+// COMSC-210 | Lab 37 | Barsbek
 #include <iostream>
+#include <string>
 using namespace std;
+
+int sum_ascii(string input);
+
 int main() {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
-    return 0;
+
+}
+
+int sum_ascii(string text) {
+    int total{};
+
 }
 /*
 These targets are present in the dataset and can be used for testing:
