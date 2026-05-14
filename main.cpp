4b09c28ae79081f6bcb0e2763b32c05eeb82c3ca
Barsbek Azamatov <bars.moderside@gmail.com>
2026-05-14 15:26:33 -0700

    Milestone 3

--- main.cpp
+++ main.cpp
@@ -7,17 +7,23 @@
 
 using namespace std;
 
-const int BUCKET_RANGE{500};
+const int BUCKET_RANGE{500};    // total bucket count
+const int NUM_FIRST_ENTRIES{100};
 
 int get_hash_index(const string& text);
 
 int main() {
+    // open dataset file
     ifstream fileInput{"lab-37-data-3.txt"};
+
+    // make sure file opened
     if (!fileInput) {
         cerr << "File not found\n";
         return 1;
     }
 
+    // key = bucket num
+    // value = list of txt codes
     map<int, list<string>> hashTable;
     string inputText;
 
@@ -26,6 +32,20 @@ int main() {
         hashTable[hashIndex].push_back(inputText);
     }
 
+    int counter{};
+    // print hash table
+    for (const auto& pair : hashTable) {
+        cout << pair.first << ": ";               // print bucket num
+        for (const auto& each : pair.second) {
+            cout << each << ' ';                  // print each txt code
+        }
+        cout << '\n';
+
+        ++counter;
+
+        if (counter >= NUM_FIRST_ENTRIES)
+            break;
+    }
 
     fileInput.close();
 
@@ -36,9 +56,9 @@ int get_hash_index(const string& text) {
     int total{};
 
     for (char each : text)
-        total += (int)each;
+        total += (int)each;              // add ascii values
 
-    int hashIndex{total % BUCKET_RANGE};
+    int hashIndex{total % BUCKET_RANGE}; // generate bucket num
 
     return hashIndex;
 }
